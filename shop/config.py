1f68c17e6c2e00d633e92d2c62c7d98d4a828d2a
Artem <lngaart55@gmail.com>
2021-06-28 02:39:07 +0300

    shop

--- shop/config.py
+++ shop/config.py
@@ -3,4 +3,4 @@ from secrets import token_hex
 
 class Config(object):
     SQLALCHEMY_DATABASE_URI = "sqlite:///shop.db"
-    SECRET_KEY = token_hex(20)
+    SECRET_KEY = token_hex(20)  # генерация секретного ключа в 16 СС
